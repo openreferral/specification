dfc2bfbcf473f001df0cf1cfa8bcce9e43055c84
Anselm Bradford <git@anselmbradford.com>
2014-03-19 23:59:39 -0700

    Adds grunt tasks for PDF generation
    Adds tasks to generate a PDF from openreferral.md markdown. The
    generated PDF appears in ./pdf/ and includes the git commit ID in the
    filename.

---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-markdown-pdf');
+	grunt.loadNpmTasks('grunt-git-rev-parse');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-rename');
+
+	grunt.initConfig(
+	{
+		pkg: grunt.file.readJSON('package.json'),
+    "git-rev-parse": {
+      build: {
+		    options: {
+		      prop: 'git-revision',
+		      number: 6
+		    }
+		  }
+    }, // git-rev-parse
+
+		copy:
+		{
+	    files: {expand: true, src: ['openreferral.md'], dest: "tmp", filter: 'isFile'}
+		}, // copy
+
+		rename:
+		{
+			files: {src: ['tmp/openreferral.md'], dest: 'tmp/openreferral-<%= grunt.config.get(\'git-revision\') %>.md'}
+    }, // rename
+
+		markdownpdf:
+		{
+			files:
+			{
+				src: "tmp/openreferral-<%= grunt.config.get('git-revision') %>.md",
+  	    dest: "pdf"
+  	  }
+		}, // markdownpdf
+
+		clean: ["tmp"], // clean
+
+	});
+
+	grunt.registerTask('pdf', ['git-rev-parse','copy','rename','markdownpdf','clean']);
+} //exports
