5caa70df8485619f9751477fa7eb88f17569cf57
Lewis Spurgin <lewisspurgin@gmail.com>
2023-01-20 18:38:10 +0000

    Revert "316 readme rtd"

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ import openreferral.svg_utils
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinxcontrib.openapi', 'sphinxcontrib.opendataservices', 'sphinx_rtd_theme',]
+extensions = ['myst_parser', 'sphinxcontrib.openapi', 'sphinxcontrib.opendataservices']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -128,15 +128,13 @@ todo_include_todos = False
 # html_theme = 'alabaster'
 html_static_path = ['_static']
 
-#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-#
-#if not on_rtd:  # only import and set the theme if we're building docs locally
-#  import sphinx_rtd_theme
-#  html_theme = 'sphinx_rtd_theme'
-#  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#  html_style = 'css/custom.css'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme = 'sphinx_rtd_theme'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_rtd_theme
+  html_theme = 'sphinx_rtd_theme'
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+  html_style = 'css/custom.css'
 
 html_context = { 
     'css_files': [
@@ -184,7 +182,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -431,7 +429,7 @@ class JSONTableSchemaInclude(Directive):
                 ('Type (Format)',
                     lambda x: x['type'] + (' ({})'.format(x['format']) if 'format' in x else '')),
                 ('Description',
-                    lambda x: x.get('description', '')),
+                    lambda x: x['description']),
                 ('Required?',
                     lambda x: str(x.get('constraints', {}).get('required', False))),
                 ('Unique?',
@@ -522,7 +520,6 @@ def setup(app):
     global html_static_path
     for file in glob.glob("../api-specification/_data/api-commons/*.yaml"):
         html_static_path = html_static_path + [file]
-
 
 
 
